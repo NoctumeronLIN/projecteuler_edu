69213ae5bff7a5d445fbc87f76776f8c283fabf0
Александр Спиридонов <75347878+NoctumeronLIN@users.noreply.github.com>
2021-02-16 14:41:46 +0300

    Update Numbers that devides by 3 and 5.py
    You can now choose what numbers to use.

--- edu_1/Numbers that devides by 3 and 5.py
+++ edu_1/Numbers that devides by 3 and 5.py
@@ -10,15 +10,31 @@ sum_of_founds = 0
 for Next_num in range(min_number, max_number):
     if((Next_num % mult_first == 0) or (Next_num % mult_second == 0)):
         sum_of_founds += Next_num
-print('"The summ of founds in that case = ', +sum_of_founds)
-print('"DO you want to check 2nd case (with 1000)? Type + or -')
-decision = input()
-if decision == "-":
+print('The summ of founds in that case = ', +sum_of_founds)
+print('DO you want to check another range? Type + or -')
+decision_another_range = input()
+if decision_another_range == "-":
     print('See you next time')
 else:
-    max_number = 1000
+    new_max_number = int(input())
+    max_number = new_max_number
     sum_of_founds = 0
-    for Next_num in range(min_number, max_number):
-        if((Next_num % mult_first == 0) or (Next_num % mult_second == 0)):
-            sum_of_founds += Next_num
-    print('"The summ of founds in that case = ', +sum_of_founds)
+    print('Maybe you should try another multiplies? Type + or -')
+    decision_another_mult = input()
+    if decision_another_mult == '-':
+        print('Ok, 3 and 5, as in problem')
+        for Next_num in range(min_number, max_number):
+            if((Next_num % mult_first == 0) or (Next_num % mult_second == 0)):
+                sum_of_founds += Next_num
+        print('The summ of founds in that case = ', +sum_of_founds)
+    else:
+        new_mult_first = int(input())
+        mult_first = new_mult_first
+        new_mult_second = int(input())
+        mult_second = new_mult_second
+        print('So, the problem now: \n "In this task we found summ between natural numbers from 1 to', +max_number, ', multiples of ', +mult_first, ' and ', +mult_second)
+        sum_of_founds = 0
+        for Next_num in range(min_number, max_number):
+            if((Next_num % mult_first == 0) or (Next_num % mult_second == 0)):
+                sum_of_founds += Next_num
+        print('The summ of founds in that case = ', +sum_of_founds)
